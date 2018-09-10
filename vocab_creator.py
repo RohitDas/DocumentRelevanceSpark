d719a000bed1a26657378089fadc3b2e8c75b43b
rohit <rohitangsudas91@gmail.com>
2018-09-11 03:51:14 +0800

    Preprocessor tested

--- vocab_creator.py
+++ vocab_creator.py
@@ -1,9 +1,35 @@
 """
     This class takes an PipeLinedRDD of the (DOCID, DOCSTRING) and creates a vocab of words.
 """
+import os
+import re
 
 class Preprocessor(object):
-    pass
+    def __init__(self, stopwords_fp):
+        self.stopwords_fp = stopwords_fp
+        self.is_stopwords_loaded = False
+
+    def load_stopwords(self):
+        if not os.path.exists(self.stopwords_fp):
+            print("Error! Stopwords file doesn't exist. ")
+        with open(self.stopwords_fp, 'r') as fp:
+            stopwords = map(lambda x: x.strip(), fp.readlines())
+        self.stopwords = set(stopwords)
+    
+    def tokenize(self, docstring):
+        if not self.is_stopwords_loaded:
+            self.load_stopwords()
+            self.is_stopwords_loaded = True
+        
+        tokens = filter(lambda x: x!='', re.split("\\W+", docstring.lower()))
+        return list(set(tokens).difference(self.stopwords))
+
+    def preprocess(self, token):
+        """
+            The Preprocess logic will be added later
+        """
+        return token
+
 
 class VocabularyCreator(object):
     def __init__(self, 
@@ -12,15 +38,22 @@ class VocabularyCreator(object):
 
     def get_vocab(self, docid_to_docstring_rdd):
         #Tokenize the documents
+        docids = docid_to_docstring_rdd.map(lambda x: x[0])
         tokenized_doc = docid_to_docstring_rdd.map(lambda x: self.preprocessor.tokenize(x[1]))
         #Undergo furhter preprocessing
         preprocessed_doc = tokenized_doc.map(lambda x: self.preprocessor.preprocess(x))
         #Take the distinct words
         vocab = preprocessor_doc.distinct()
-        return vocab
+        return docids, vocab
 
+if __name__ == "__main__":
+    #Checking Preprocessor
+    stopwords_fp = "/home/rohittulu/Downloads/stopwords.txt"
+    preprocessor = Preprocessor(stopwords_fp)
+    preprocessor.load_stopwords()
+    print preprocessor.tokenize("Virat kohli played well but the other players sucked big time.")
 
-
-
+    #Checking Vocabulary creator
+    vocab_creator = VocabularyCreator(preprocessor)
 
 
