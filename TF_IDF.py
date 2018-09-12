dee15014795c975c5c6943580e5a5557724a583f
rohit <rohitangsudas91@gmail.com>
2018-09-13 06:09:50 +0800

    representation done, and cosine similarity added

--- TF_IDF.py
+++ TF_IDF.py
@@ -7,6 +7,18 @@ import os, re, math
 from pyspark import SparkContext, SparkConf
 
 STOPWORDS = set()
+VOCAB = None
+
+
+def dot_prod(vector_a, vector_b):
+    vec_len = len(vector_a)
+    dot_prod = 0
+    for i in range(vec_len):
+        dot_prod += vector_a[i]*vector_b[i]
+    return dot_prod
+
+def cosine(vector_a, vector_b):
+    return dot_prod(vector_a, vector_b)/(math.sqrt(dot_prod(vector_b, vector_b))*math.sqrt(dot_prod(vector_b, vector_b)))
 
 def load_stopwords(stopwords_fp):
     if not os.path.exists(stopwords_fp):
@@ -20,6 +32,11 @@ def tokenize(token_string):
     tokens = filter(lambda x: x!='', re.split("\\W+", token_string.lower()))
     return list(set(tokens).difference(STOPWORDS))
 
+def normalize(word_tf_idf_list):
+    #Sum of squares
+    S = math.sqrt(reduce(lambda x,y: x+y, map(lambda a: math.pow(a[1],2), word_tf_idf_list)))
+    return map(lambda x: (x[0], x[1]/S), word_tf_idf_list)
+
 class TFIDFCalcular(object):
     def __init__(self,
                  context,
@@ -31,6 +48,7 @@ class TFIDFCalcular(object):
         self.stopwords_path = stopwords_path
         self.sample_factor = sample_factor
         self.is_objs_initialized = False
+        self.doc_representation = None
 
     def initialize_objs(self):
         self.data_loader = DataLoader(self.context)
@@ -62,7 +80,7 @@ class TFIDFCalcular(object):
         doc_ids_to_doc_str = self.data_loader.load_data_under_cur_context(self.data_path,
                                                                           self.sample_factor)
         docids, vocab = self.vocab_creator.get_vocab(doc_ids_to_doc_str)
-
+        VOCAB = vocab.collect()
         cartesian_docs = self.get_cartesian_rdd(docids,
                                                 vocab)
         tf = self.calculate_tf(doc_ids_to_doc_str)
@@ -71,14 +89,20 @@ class TFIDFCalcular(object):
         total_docs = doc_ids_to_doc_str.count()
         #Calculating tf_idf
         tf_idf = tf.map(lambda x: (x[0], (1+math.log(df.get(x[0][1]))*math.log(total_docs/x[1]))))  
-        #Merging cartesian docs with merged tf_idf
-        #merged_tf_idf = tf_idf.union(cartesian_docs)
         #Sort according to the keys
         sorted_merged_tf_idf = tf_idf.sortByKey()
         #Group by key to form a vector
         representation = sorted_merged_tf_idf.map(lambda x: (x[0][0], (x[0][1], x[1]))).combineByKey(lambda x: [x], lambda u,v: u+[v], lambda u1, u2: u1+u2)
         #grouped_tf_idf = sorted_merged_tf_idf.groupByKey(lambda x: [x], lambda u,v: u+[v], lambda u1, u2: u1+u2)
-        return representation
+        normalize_representation = representation.map(lambda x: (x[0], normalize(x[1])))
+        self.doc_representation =  normalize_representation
+        return normalize_representation
+
+    def query(self, sentence):
+        """
+            Get the top k similar documents
+        """
+        pass
 
         
 if __name__ == "__main__":
